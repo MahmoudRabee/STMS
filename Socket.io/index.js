07b58513165ac48e9f299c147d3b08fb6a5dcf58
Mahmoud <mrabee63@gmail.com>
2020-08-04 19:59:59 +0200

    almost done

--- Socket.io/index.js
+++ Socket.io/index.js
@@ -3,7 +3,7 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/control.html');
 });
 
 // io.on('connection', (socket) => {
@@ -13,8 +13,18 @@ io.on('connection', (socket) => {
     console.log("connected");
     socket.on('chat message', (msg) => {
       console.log('message: ' + msg);
+ 
     });
-    socket.emit('Hi', "Hello, from server");
+    setInterval(emmit,3000);
+    
+   function emmit() {
+     socket.emit('car numbers', {
+      a1:1,
+      a2:1,
+      b1:1,
+      b2:1
+    });
+   }
   });
 http.listen(3000, () => {
   console.log('listening on *:3000');
